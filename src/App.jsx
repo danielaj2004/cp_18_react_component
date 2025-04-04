9742a1f0ac3fa87134dceb3adcfccd5d5347ff36
danielaj2004 <danielajoya12@gmail.com>
2025-04-04 17:45:23 -0400

    Component integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+import Greeting from './components/Greeting'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,9 +23,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Greeting />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
